38f0bf708d5ce66c9494f5194506b0c4442f64c5
indudevi1000105-sudo <indu.devi1000105@gmail.com>
2026-02-15 00:26:25 +0530

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,14 @@
 import '../../styles/globals.css';
 
-export default async function RootLayout({ children }) {
+export const metadata = {
+  verification: {
+    google: '23kAS2g1VN7deKZLKIOmQHkJGkMXhocngVwF5xJD58s',
+  },
+};
+
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head><meta name="google-site-verification" content="23kAS2g1VN7deKZLKIOmQHkJGkMXhocngVwF5xJD58s" /></head>
       <body>
         {children}
       </body>
