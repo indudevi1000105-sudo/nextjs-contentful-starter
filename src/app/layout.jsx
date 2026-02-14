78f206c3ecdf20dddf8b9765c2b6547ebdd506d1
indudevi1000105-sudo <indu.devi1000105@gmail.com>
2026-02-15 00:21:25 +0530

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,7 @@ import '../../styles/globals.css';
 export default async function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head><meta name="google-site-verification" content="23kAS2g1VN7deKZLKIOmQHkJGkMXhocngVwF5xJD58s" /></head>
       <body>
         {children}
       </body>
